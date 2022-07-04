1ac2166d1bbf17dddf5371c46b3d6b5d1491ec6e
ArdaOdabasi <61662021+ArdaOdabasi@users.noreply.github.com>
2022-07-04 14:45:27 +0300

    Successfully Completed
    The project was completed successfully.

---
+++ Helpers.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Helpers.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Helpers.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4a3f83f7-733b-4f9e-b0a8-4e9bafb816a8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Helpers.Tests/StringHelperTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Tests
+{
+    [TestClass]
+    public class StringHelperTests
+    {
+        [TestMethod]
+        public void Girilen_ifadenin_basindaki_ve_sonundaki_fazla_bosluklar_silinmelidir()
+        {
+            //Arrange
+            var ifade = "   Arda Odabaşı  ";
+            var beklenen = "Arda Odabaşı";
+
+            //Act
+            var gerceklesen = StringHelper.FazlaBosluklariSil(ifade);
+
+            //Assert
+            Assert.AreEqual(beklenen,gerceklesen);
+        }
+
+        [TestMethod]
+        public void Girilen_ifadenin_icindeki_fazla_bosluklar_silinmelidir()
+        {
+            //Arrange
+            var ifade = "Arda Odabaşı  Ersun     Yanal Alex De   Souza";
+            var beklenen = "Arda Odabaşı Ersun Yanal Alex De Souza";
+
+            //Act
+            var gerceklesen = StringHelper.FazlaBosluklariSil(ifade);
+
+            //Assert
+            Assert.AreEqual(beklenen, gerceklesen);
+        }
+    }
+}
